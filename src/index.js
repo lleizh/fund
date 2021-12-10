e2fe18ee29649f0ffd243bb8f70a073974b1bc2d
lleizh <zhang_leilei@aja-kk.co.jp>
2021-12-10 19:01:50 +0900

    template

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+// scroll bar
+import 'simplebar/src/simplebar.css';
+
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+
+//
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
+
+// ----------------------------------------------------------------------
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
+  <HelmetProvider>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+    </BrowserRouter>
+  </HelmetProvider>,
+  document.getElementById('root')
 );
+
+// If you want to enable client cache, register instead.
+serviceWorker.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
